c0ca5f0165b440a23db81d7a07e34af130e6a122
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-20 23:53:13 +0400

    Worked on the bosy layout

---
+++ src/components/body/Airbnb.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./airbnb.scss";
+
+const Airbnb = () => {
+  return (
+    <div className="airbnb">
+      <div className="item">
+        <div className="images">
+          <img
+            src="https://www.mymove.com/wp-content/uploads/2013/08/GettyImages-597316425-scaled.jpg"
+            alt="l"
+          />
+        </div>
+        <div className="details"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Airbnb;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../components/header/Header";
 import Filter from "../components/filter/Filter";
+import Airbnb from "../components/body/Airbnb";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Header />
+
       <Filter />
+      <Airbnb />
     </div>
   );
 };
