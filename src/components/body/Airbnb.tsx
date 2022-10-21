3927d17257bb7b3ad7fbb3f5a764d29f391dbb01
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-21 18:05:22 +0400

    positioned the card items how they should be

--- src/components/body/Airbnb.tsx
+++ src/components/body/Airbnb.tsx
@@ -1,17 +1,81 @@
 import React from "react";
 import "./airbnb.scss";
+import { AiFillHeart } from "react-icons/ai";
+import { FcLike } from "react-icons/fc";
 
 const Airbnb = () => {
   return (
     <div className="airbnb">
       <div className="item">
-        <div className="images">
-          <img
-            src="https://www.mymove.com/wp-content/uploads/2013/08/GettyImages-597316425-scaled.jpg"
-            alt="l"
-          />
+        <div className="top">
+          <AiFillHeart className="like-icon" />
+
+          <div className="images">
+            <img
+              src="https://bluprint.onemega.com/wp-content/uploads/2018/03/myhome-tropical-house-image-4.jpg"
+              alt="l"
+            />
+          </div>
         </div>
-        <div className="details"></div>
+
+        <div className="details-container">
+          <div className="details"></div>
+          <div className="start"></div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="top">
+          <AiFillHeart className="like-icon" />
+
+          <div className="images">
+            <img
+              src="https://bluprint.onemega.com/wp-content/uploads/2018/03/myhome-tropical-house-image-4.jpg"
+              alt="l"
+            />
+          </div>
+        </div>
+
+        <div className="details-container">
+          <div className="details"></div>
+          <div className="start"></div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="top">
+          <AiFillHeart className="like-icon" />
+
+          <div className="images">
+            <img
+              src="https://bluprint.onemega.com/wp-content/uploads/2018/03/myhome-tropical-house-image-4.jpg"
+              alt="l"
+            />
+          </div>
+        </div>
+
+        <div className="details-container">
+          <div className="details"></div>
+          <div className="start"></div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="top">
+          <AiFillHeart className="like-icon" />
+
+          <div className="images">
+            <img
+              src="https://bluprint.onemega.com/wp-content/uploads/2018/03/myhome-tropical-house-image-4.jpg"
+              alt="l"
+            />
+          </div>
+        </div>
+
+        <div className="details-container">
+          <div className="details"></div>
+          <div className="start"></div>
+        </div>
       </div>
     </div>
   );
