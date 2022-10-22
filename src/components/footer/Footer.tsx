46c20f6a33fa9a0a45c85ec1a7231781a3f07ee2
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-10-22 07:26:11 +0400

    Finished implementing the footer UI

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { Language } from "@mui/icons-material";
+import { FaChevronUp } from "react-icons/fa";
 import "./footer.scss";
 
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer: React.FC = () => {
+  return (
+    <div className="footer">
+      <div className="left">
+        <p>© 2022 Airbnb, Inc.</p>
+        <ul>
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Sitemap</li>
+        </ul>
+      </div>
+      <div className="right">
+        <div>
+          <Language className="language" />
+          <h4>English (US)</h4>
+        </div>
+
+        <div>
+          <b>$</b>
+          <h4> USD</h4>
+        </div>
+
+        <div>
+          <h4>Support & resources</h4>
+          <FaChevronUp className="chevron-up" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
